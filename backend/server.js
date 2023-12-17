49e9e4f6c7e5c168956b43f274d76f19b98f027e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2023-12-17 21:03:51 +0000

    added custom logger with morgan and winston

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import chalk from 'chalk';
 import morgan from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { morganMiddleware, systemLogs } from './utils/logger.js';
 import 'dotenv/config';
 
 const app = express();
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(morganMiddleware);
 
 app.get('/api/v1/test', (req, res) => {
   return res.json({ status: 'it works!!!' });
@@ -24,5 +26,8 @@ app.listen(PORT, () => {
     `${chalk.green.bold('✔')} 👍 server running in ${chalk.yellow.bold(
       process.env.NODE_ENV
     )} mode on port ${chalk.blue.bold(PORT)}`
+  );
+  systemLogs.info(
+    `server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
   );
 });
