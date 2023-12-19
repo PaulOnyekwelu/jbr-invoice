44a6be0ce11aca5ba1665e3c8823ef6770ffd0e7
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2023-12-19 18:38:03 +0000

    setup sendemail and email templates

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import handlebars from 'handlebars';
+
+import transporter from '../helpers/emailTransporter.js';
+import { systemLogs } from './logger.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const sendEmail = async (email, subject, payload, template) => {
+  try {
+    const srcDir = fs.readFileSync(path.join(__dirname, template));
+    const compiledTemplate = handlebars.compile(srcDir);
+
+    const emailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject,
+      html: compiledTemplate(payload),
+    };
+
+    await transporter.sendEmail(emailOptions);
+  } catch (error) {
+    systemLogs(`Email not sent: ${error}\n`);
+  }
+};
