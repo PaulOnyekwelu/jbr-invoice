6282baa299830b5ee291dc725a988305e48e8694
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2023-12-19 13:30:56 +0000

    configure nodemailer email helper

---
+++ backend/utils/emailTransporter.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import nodemailer from 'node-mailer';
+
+let transporter;
+
+if (process.env.NODE_ENV === 'development')
+  transporter = nodemailer.createTransport({
+    host: 'mailhog',
+    port: 1025,
+  });
+
+if (process.env.NODE_ENV === 'production')
+  transporter = nodemailer.createTransport({
+    // @TODO: SETUP EMAIL TRANSPORTER FOR PRODUCTION USING MAILGUN
+  });
+
+export default transporter;
